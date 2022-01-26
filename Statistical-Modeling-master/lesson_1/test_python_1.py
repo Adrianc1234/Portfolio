d19f9e147c469d18e54a92734c8016b8a14b1309
艾德里安 <46505958+Adrianc1234@users.noreply.github.com>
2022-01-26 10:29:43 -0600

    Add files via upload

---
+++ Statistical-Modeling-master/lesson_1/test_python_1.py
@@ -0,0 +1,7 @@
+Name = raw_input("please write your name and your last name: ")
+iceCreamRating= int(input("give me your opinion since 0 until 10, 10 is the better qualification: "))
+sleepingIndex=float(input("give me a number between 0.0 and 1.0, how much do you like to sleep?: "))
+print("\n My name is " + str(Name) + " and I give eating ice cream a score of "+ str(iceCreamRating) + " out of 10!\n"
+" I am " + str(Name) + " and my sleeping enjoyment index is " + str(sleepingIndex) + " out of 1.0!")
+
+         
